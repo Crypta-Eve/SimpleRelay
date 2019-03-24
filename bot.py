1ea61cb7530e4bb25106055da20c7e01d7a386f6
Crypta-Eve <crypta@crypta.tech>
2019-03-24 15:22:02 +1100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+import config
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.channel.id in config.channel_relay:
+        channel = client.get_channel(config.channel_relay[message.channel.id])
+        msg = message.author.nick + '\n' + message.content
+        await channel.send(msg)
+
+
+
+client.run(config.bot_token)
+
+
